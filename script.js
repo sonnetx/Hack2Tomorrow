70ef5b74474fa4c74a41bf9c9a6b89680765b073
takore05 <60117058+takore05@users.noreply.github.com>
2020-07-25 20:57:21 -0400

    update script

--- script.js
+++ script.js
@@ -9,19 +9,22 @@ function checkPasswordStrength(password){
   else{
     points += 0;
   }
-
+  var special = "*|,\":<>[]{}`\';()@&$#%";
 
   for(let i=0;i<password.length;i++){
     if(password.charAt(i)>='0' && password.charAt(i)<='9'){ //checks if numbers
       points++;
     }else if(password.charAt(i)>='A' && password.charAt(i)<='Z'){ //checks if uppercase
       points++;
+    } else if(special.indexOf(password.charAt(i))!=-1){
+      points++;
     } else{ 
       points+=0.5;
     }
   //need to check for special characters
   }
-  if(points<25){
+  //if points are below the minimum strong password, lifetime for password cut in half
+  if(points<5){
     points = points/2;
   }
   var days = points * 4.5;
